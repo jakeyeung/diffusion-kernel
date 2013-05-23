0146510a58bbff23eeebc08e2a0c0b8a838431dd
jakeyeung <jakeyeung@gmail.com>
2013-05-23 13:43:54 -0700

    Converted filenames into arguments to be put into commandline.

--- R/get_influence_from_network.R
+++ R/get_influence_from_network.R
@@ -2,11 +2,19 @@
 # May 11 2013
 # get_influence_hprd_network.R
 # Uses diffusion model to analyze HPRD network
+# Usage in commandline: 
+# Rscript get_influence_hprd_network.R HPRD_network_filename.txt table_output_fname.txt influence_list.txt plotfname.pdf
 
 
 # SetCommandArgs ----------------------------------------------------------
 
 args <- commandArgs(trailingOnly=TRUE)
+filename <- args[1]
+diff_full_table_fname <- args[2]
+diff_total_fname <- args[3]
+influence_degree_plot_fname <- args[4]
+
+
 
 
 # LoadLibraries -----------------------------------------------------------
@@ -39,7 +47,7 @@ source(file.path(sourceDir, 'NormalizeInfluence.R'))
 gamma <- 1    # Rate of "sink" in each node for diffusion model.
 
 if (is.na(args[1]) == TRUE){
-    filename <- 'HPRD_NAgene.txt'
+    filename <- 'HPRD_noNA.txt'
     print(paste('No argument specified, using', 
                 filename, 'as network filename'))
 } else{
@@ -100,43 +108,46 @@ sum_diffusion_matrix.norm <- rowSums(influence_matrix_diffusion.norm)
 # SaveDiffusionInfluence --------------------------------------------------
 
 write.table(signif(influence_matrix_diffusion, 3), 
-            file=file.path(textDir, 'diffusion_influence_hprd.txt'), sep='\t')
+            file=file.path(textDir, diff_full_table_fname), sep='\t')
 
-write.table(signif(influence_matrix_diffusion.norm, 3), 
-            file=file.path(textDir, 'diffusion_influence_hprd.txt'), sep='\t')
+# write.table(signif(influence_matrix_diffusion.norm, 3), 
+#             file=file.path(textDir, 'diffusion_influence_hprd.txt'), sep='\t')
 
 
-# SaveTotalInfluence -------------------------------------------------------------
+# TotalInfluence -------------------------------------------------------------
 
 sum_diff_mat_table <- as.data.frame(sum_diffusion_matrix)
 colnames(sum_diff_mat_table) <- 'total_influence'
-write.table(sum_diff_mat_table, 
-            file=file.path(textDir, 'total_influence.txt'), sep='\t')
 
-sum_diff_mat_table.norm <- as.data.frame(sum_diffusion_matrix.norm)
-colnames(sum_diff_mat_table.norm) <- 'total_influence'
-write.table(sum_diff_mat_table.norm, 
-            file=file.path(textDir, 'total_influence.txt'), sep='\t')
+# sum_diff_mat_table.norm <- as.data.frame(sum_diffusion_matrix.norm)
+# colnames(sum_diff_mat_table.norm) <- 'total_influence'
+# write.table(sum_diff_mat_table.norm, 
+#             file=file.path(textDir, 'total_influence.txt'), sep='\t')
 
 
 # AddNodeDegreesToTable ------------------------------------------
 
 sum_diff_mat_table <- cbind(sum_diff_mat_table, 'node_degree'=degree(g))
-sum_diff_mat_table.norm <- cbind(sum_diff_mat_table.norm, 
-                                 'node_degree'=degree(g))
+# sum_diff_mat_table.norm <- cbind(sum_diff_mat_table.norm, 
+#                                  'node_degree'=degree(g))
 
+
+# SaveTotalInfluenceNodeDegreeTable ---------------------------------------
+
+write.table(sum_diff_mat_table, 
+            file=file.path(textDir, diff_total_fname), sep='\t')
 
 # PlotInfluencevsNodeDegree -----------------------------------------------
 
 p <- qplot(node_degree, total_influence, data=sum_diff_mat_table, alpha=I(1/50), 
            xlab='Node Degree', ylab='Total Influence', 
            main='Total Influence vs Node Degree') + scale_x_log10()
-ggsave(file=file.path(plotDir, 'tot_inf_vs_node_degree.pdf'), plot=p)
+ggsave(file=file.path(plotDir, influence_degree_plot_fname), plot=p)
 
-p <- qplot(node_degree, total_influence, data=sum_diff_mat_table, alpha=I(1/50), 
-           xlab='Node Degree', ylab='Normalized Total Influence', 
-           main='Normalized Total Influence vs Node Degree') + 
-        scale_x_log10()
-ggsave(file=file.path(plotDir, 'normalized_tot_inf_vs_node_degree.pdf'), plot=p)
+# p <- qplot(node_degree, total_influence, data=sum_diff_mat_table, alpha=I(1/50), 
+#            xlab='Node Degree', ylab='Normalized Total Influence', 
+#            main='Normalized Total Influence vs Node Degree') + 
+#         scale_x_log10()
+# ggsave(file=file.path(plotDir, 'normalized_tot_inf_vs_node_degree.pdf'), plot=p)
 
 
