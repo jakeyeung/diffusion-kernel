4c72d2d5f8ca626801188cbd5ff6bd5afd3b86ba
jakeyeung <jakeyeung@gmail.com>
2013-05-24 11:23:51 -0700

    Created a class to manage influence matrices. Created initial main to work with two influence matrices.

--- python_diff_kernel/calc_correlation_coefficients.py
+++ python_diff_kernel/calc_correlation_coefficients.py
@@ -5,18 +5,51 @@ Created on 2013-05-23
 '''
 
 
-from utilities import set_directories
+import os
+import sys
+from utilities import set_directories, influence
+# from scipy import stats
+
 
 # Set directories
-'''
-_cur_dir = os.path.dirname(os.path.realpath(__file__))
-_upthree_dir = os.path.dirname(os.path.dirname(os.path.dirname(_cur_dir)))
-_input_dir = os.path.join(_upthree_dir, 'inputs')
-_output_dir = os.path.join(_upthree_dir, 'outputs')
-_plot_dir = os.path.join(_output_dir, 'plots')
-'''
+path = set_directories.paths('input', 'output')
+
+# Set constants
+hprdfolder = 'hprd'
+textoutputfolder = 'text_outputs'
+# influence_filename1 = 'hprd_full_table3.txt'
+# influence_filename2 = 'HPRD_InfluenceGraph_HotNet.txt'
 
 
 if __name__ == '__main__':
-    directories = set_directories.paths('curdir')
-    print directories.curdir
+    if len(sys.argv) < 3:
+        print('Two influence matrices must be provided in command line.')
+        sys.exit()
+    
+    # Define filenames
+    influence_filename1 = sys.argv[1]
+    influence_filename2 = sys.argv[2]
+    
+    # Define full path names
+    influence_fpath1 = os.path.join(path.outputdir, hprdfolder, textoutputfolder, influence_filename1)
+    influence_fpath2 = os.path.join(path.outputdir, hprdfolder, textoutputfolder, influence_filename2)
+    
+    # Create two classes
+    influence1 = influence.diffusion_influence(influence_fpath1)
+    influence2 = influence.diffusion_influence(influence_fpath2)
+    
+    # Open the two files, create .reader instances
+    influence1.openfile()
+    influence2.openfile()
+    
+    while True:
+        try:
+            print ('Influence 1 row: %s, length: %s' %(influence1.readnextline()[0:5], len(influence1.readnextline())))
+            print ('Influence 2 row: %s, length: %s' %(influence2.readnextline()[0:5], len(influence2.readnextline())))
+            raw_input('Paused. Enter to continue...')
+        except StopIteration:
+            print('No more rows to iterate, breaking...')
+            break
+            
+    influence1.closefile()
+    
---
+++ python_diff_kernel/utilities/influence.py
@@ -0,0 +1,39 @@
+'''
+Created on 2013-05-24
+
+@author: jyeung
+'''
+
+
+import csv
+
+
+class diffusion_influence(object):
+    '''
+    Represents a diffusion influence matrix file.
+    '''
+    def __init__(self, full_path):
+        '''
+        Initialize as a full path
+        '''
+        self.path = full_path
+        self.rowsread = 0
+    
+    def openfile(self):
+        csvfile = open(self.path, 'rb')
+        self.reader = csv.reader(csvfile, delimiter='\t')
+        return self.reader
+    
+    def readnextline(self):
+        self.rowsread += 1
+        return self.reader.next()
+    
+    def closefile(self):
+        '''
+        Should only be initiated after the file has opened (readnextline)
+        has been called. Closes the file to tie up loose ends.
+        '''
+        self.reader.close()
+        
+        
+        
