cb1d6db3d904eae137bbc31fe4bc416720ef7411
jakeyeung <jakeyeung@gmail.com>
2013-05-30 13:30:58 -0700

    Imports 'power series expansion' function and calculates both exact inverse laplacian and the approximated inverse laplacian.
    Plots both results for user to see the difference.

--- R/truncated_vs_full_diffusion.R
+++ R/truncated_vs_full_diffusion.R
@@ -28,6 +28,7 @@ source(file=file.path(sourceDir, 'GetHubDf.R'))
 
 # Diffuion Kernel Functions
 source(file=file.path(sourceDir, 'SolveShiftedLaplacian.R'))
+source(file=file.path(sourceDir, 'power_series_expansion.R'))
 source(file=file.path(sourceDir, 'GetSteadyStateDiffusion.R'))
 
 
@@ -50,6 +51,8 @@ if (hub_size<2){
 }
 gamma <- 1    # Rate of "sink" in each node for diffusion model.
 
+set.seed(1)    # Random weights
+
 
 # InitializeGraph ---------------------------------------------------------
 
@@ -57,11 +60,74 @@ gamma <- 1    # Rate of "sink" in each node for diffusion model.
 df_line <- GetLineDf(line_length)
 df_hub <- GetHubDf(hub_size, line_length)
 df_scorpion <- rbind(df_line, df_hub)
+colnames(df_scorpion) <- c('V1', 'V2')
 
-g <- GetGraphFromDf(df_scorpion)
-plot(g)
-A <- get.adjacency(g)    # Adjacency matrix
-L <- graph.laplacian(g)    # Laplacian matrix
+
+# AddWeights --------------------------------------------------------------
+
+weight <- runif(nrow(df_scorpion))
+weights_hub <- runif(nrow(df_hub))
+# weights_hub <- rep(1, nrow(df_hub))
+df_scorpion <- cbind(df_scorpion, weight)
+df_hub <- cbind(df_hub, weights_hub)
+colnames(df_hub) <- c('V1', 'V2', 'weight')
+df_hub[, 1] <- df_hub[, 1] - 3
+df_hub[, 2] <- df_hub[, 2] - 3
+
+g <- graph.data.frame(data.frame(df_scorpion), directed=FALSE, vertices=NULL)
+# g <- graph.data.frame(data.frame(df_hub), directed=FALSE, vertices=NULL)
+
+# g <- GetGraphFromDf(df_scorpion)
+plot(g, edge.width=E(g)$weight*10)
+
+
+A <- get.adjacency(g, attr='weight')    # Adjacency matrix
+L <- graph.laplacian(g, weights=E(g)$weights)    # Laplacian matrix
 D <- diag(diag(L))    # Diagonal matrix
 
+
+
+# CalculateExactL ---------------------------------------------------------
+
+Linv <- SolveShiftedLaplacian(A, D, gamma)
+influence_matrix_diffusion <- GetSteadyStateDiffusion(Linv, 
+                                                      remove_diags=FALSE)
+
+
+# CalculateApproximateL ---------------------------------------------------
+
+Linv_approx <- power_series_expansion(A, D, gamma, 10)
+influence_matrix_diffusion_approx <- GetSteadyStateDiffusion(Linv_approx, 
+                                                             remove_diags=TRUE)
+
+
+# PlotResults -------------------------------------------------------------
+
+# Plot parameters: diffusion
+xlabel='Ranked node'
+ylabel='Influence'
+xmin <- 0
+xmax <- nrow(influence_matrix_diffusion)
+ymin <- 0
+ymax <- max(influence_matrix_diffusion)*1.2
+title <- 'Diffusion Influence Graph: Linv Exact'
+
+plotInfluence(influence_matrix_diffusion, xlabel, ylabel, xmin, xmax, ymin, 
+              ymax, title, saveDir=plotDir, 
+              saveFileName='diffusion_influence_graph_test1.pdf', 
+              sortdecreasing=TRUE)
+
+# Plot parameters: diffusion
+xlabel='Ranked node'
+ylabel='Influence'
+xmin <- 0
+xmax <- nrow(influence_matrix_diffusion)
+ymin <- 0
+ymax <- max(influence_matrix_diffusion)*1.2
+title <- 'Diffusion Influence Graph: Linv Approx'
+
+plotInfluence(influence_matrix_diffusion_approx, xlabel, ylabel, xmin, xmax, ymin, 
+              ymax, title, saveDir=plotDir, 
+              saveFileName='diffusion_influence_graph_test2.pdf', 
+              sortdecreasing=TRUE)
 
