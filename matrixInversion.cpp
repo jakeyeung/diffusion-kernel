104b687ff3a5c5404df38cb9bc0e55893c73ce78
ehodzic <ermin.h.hodzic@gmail.com>
2013-06-13 14:52:30 -0700

    Transformed to in-place inversion. Added singularity check.

--- matrixInversion.cpp
+++ matrixInversion.cpp
@@ -1,20 +1,21 @@
-void invert( double ** const A, double ** const B, int n ) {
-	for ( int i = 0; i < n; i++ ) {
-		for ( int j = 0; j < i; j++ ) B[ i ][ j ] = 0;
-		B[ i ][ i ] = 1;
-		for ( int j = i + 1; j < n; j++ ) B[ i ][ j ] = 0;
-	}
+#define EPS 1e-10
+
+bool invertInPlace( double ** const A, int n ) {
 	for ( int k = 0; k < n; k++ ) {
+		if ( fabs( A[ k ][ k ] ) < EPS ) return 0;
+		
 		double temp = A[ k ][ k ];
-		for ( int j = k; j < n; j++ ) A[ k ][ j ] /= temp;
-		for ( int j = 0; j <= k; j++ ) B[ k ][ j ] /= temp;
+		A[ k ][ k ] = 1;
+		for ( int j = 0; j < n; j++ ) A[ k ][ j ] /= temp;
 		
 		for ( int i = 0; i < n; i++ ) {
 			if ( i == k ) continue;
 			
 			temp = -A[ i ][ k ];
-			for ( int j = k; j < n; j++ ) A[ i ][ j ] += A[ k ][ j ] * temp;
-			for ( int j = 0; j <= k; j++ ) B[ i ][ j ] += B[ k ][ j ] * temp;
+			A[ i ][ k ] = 0;
+			for ( int j = 0; j < n; j++ ) A[ i ][ j ] += A[ k ][ j ] * temp;
 		}
 	}
+	
+	return 1;
 }
